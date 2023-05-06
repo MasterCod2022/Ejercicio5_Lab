07dcecfa7c27c474569c77178877ea8650e2f23a
MasterCod2022 <tabletnew017@gmail.com>
2023-05-05 22:34:49 -0300

    Initial commit

---
+++ src/ejercicio5_lab/Arreglo.java
@@ -0,0 +1,78 @@
+
+package ejercicio5_lab;
+
+
+public class Arreglo {
+   public static void sumarLista(int arreglo[]) { 
+
+        int suma = 0; 
+        double promedio = 0; 
+        for (int i = 0; i < arreglo.length; i++) { 
+        suma += arreglo[i]; 
+
+        } 
+
+        promedio = suma / arreglo.length; 
+        System.out.println("La suma es: " + suma); 
+        System.out.println("El promedio es: " + promedio); 
+
+    } 
+
+ 
+
+    public static int buscarMayor(int array[][]) { 
+
+        int mayor = array[0][0]; 
+        for (int i = 0; i < array.length; i++) { 
+            for (int j = 0; j < array[i].length; j++) { 
+
+                if (array[i][j] > mayor) { 
+
+                    mayor = array[i][j]; 
+
+                } 
+            } 
+
+        } 
+        return mayor; 
+
+    } 
+
+ 
+    public static int cuentaVocales(String palabra) { 
+       int vocales =0; 
+
+        if (palabra != null) { 
+           
+            for (int i = 0; i < palabra.length(); i++) { 
+                if ((palabra.charAt(i)== 'a')|| (palabra.charAt(i)=='e') || (palabra.charAt(i)== 'i') || 
+                   (palabra.charAt(i)== 'o') || (palabra.charAt(i)=='u')){ 
+                    vocales++; 
+
+                } 
+
+            } 
+
+             
+        } 
+        return vocales; 
+
+    } 
+
+ 
+    public static int cuentaCaracter(String str, char ch) { 
+        int cont = 0; 
+
+         for (int i = 0; i < str.length(); i++) { 
+             if (str.charAt(i) == ch) { 
+                cont++; 
+
+            } 
+
+        } 
+        return cont; 
+
+    } 
+
+}   
+
---
+++ src/ejercicio5_lab/Ejercicio5_Lab.java
@@ -0,0 +1,38 @@
+
+package ejercicio5_lab;
+
+
+public class Ejercicio5_Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     //int arreglo[]= new int[5]; 
+
+         
+        int arreglo[]= {10,4,6}; 
+
+        /*int irregular[][]= new int [3][]; 
+        array[0] = new int[3]; 
+
+        array[1] = new int[4]; 
+
+        array[2] = new int[2];*/ 
+
+       
+        int irregular[][]= {{1,2,3},{4,5,6,7},{8,9}}; 
+       
+        Arreglo.sumarLista(arreglo); 
+
+        System.out.println("El numero mayor es: "+Arreglo.buscarMayor(irregular)); 
+
+        String palabra = "holx mundo" ; 
+        System.out.println("cantidad de vocales en el arreglo: "+Arreglo.cuentaVocales(palabra)); 
+
+
+        System.out.println("Cantidad de caracteres: "+Arreglo.cuentaCaracter(palabra, 'x')); 
+
+   } 
+
+} 
